69c2a795547359ded5f88cb8102199189896acff
francesyu90 <francesyu90@yahoo.com>
2018-03-29 01:25:50 -0700

    Now worker can consume message

--- worker/worker.go
+++ worker/worker.go
@@ -1,19 +1,96 @@
 package main
 
 import (
-	"../app/src/controller"
+	"log"
+
+	"github.com/streadway/amqp"
+
+	"../app/src/config"
+	"../app/src/consumer"
+	"../app/src/util"
 )
+
+const (
+	amqpURI = "amqp://guest:guest@localhost:5672/"
+)
+
+func createConnection() *amqp.Connection {
+
+	c := make(chan *amqp.Connection)
+
+	go func() {
+		conn, err := amqp.Dial(amqpURI)
+		if err != nil {
+			c <- nil
+		} else {
+			c <- conn
+		}
+	}()
+
+	conn := <-c
+	return conn
+}
+
+func createChannel(conn *amqp.Connection) *amqp.Channel {
+
+	c := make(chan *amqp.Channel)
+
+	go func() {
+		ch, err := conn.Channel()
+		if err != nil {
+			c <- nil
+		} else {
+			c <- ch
+		}
+	}()
+
+	ch := <-c
+	return ch
+}
+
+func createQueue(ch *amqp.Channel) *amqp.Queue {
+
+	v := config.ReadInConfig()
+	u := util.NewUtilites(v)
+	queueConfig := u.GetQueueConfig()
+
+	queue, err := ch.QueueDeclare(
+		queueConfig.Name,
+		queueConfig.Durable,
+		queueConfig.AutoDelete,
+		queueConfig.Exclusive,
+		queueConfig.NoWait,
+		nil,
+	)
+
+	if err != nil {
+		return nil
+	}
+
+	return &queue
+}
 
 func main() {
 
-	messageController := controller.GetMessageController()
-	ch := make(chan int)
+	conn := createConnection()
+	if conn == nil {
+		log.Fatalln(conn)
+	}
+
+	c := make(chan int)
 
 	go func() {
-		messageController.ConsumeMessage()
-		ch <- 0
+		ch := createChannel(conn)
+		if ch == nil {
+			log.Fatalln(ch)
+		}
+		defer ch.Close()
+		queue := createQueue(ch)
+		consumer := consumer.NewConsumer(queue)
+		consumer.Consume(ch)
+		c <- 0
 	}()
 
-	<-ch
+	<-c
 
 }
